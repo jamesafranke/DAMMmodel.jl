f7d19f68bbe3dc1030583411a77b939fd9731160
jamesafranke <jfranke@uchicago.edu>
2022-01-31 13:19:42 -0600

    Small test added to DAMMfit to check lenght of output

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,7 @@ using Test
 	x = [18.0 0.35; 22.0 0.22] # Ind variables test
 	p = (1e9, 64.0, 3.46e-8, 2.0e-3, 0.4, 0.0125, 1.0) # Parameters test
 	@test typeof(DAMM(x, p)) == Vector{Float64} # DAMM test
+
+	Ind_var = hcat([19.0, 22.0], [0.35, 0.22])
+	@test lenght( DAMMfit(Ind_var, [2.2, 2.8], 0.4) ) == 7  # new DAMM test JF
 end
